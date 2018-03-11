659069701a9584276bd1ee4055c27aa22a4052c8
efg.river <efg.river@gmail.com>
2018-03-11 16:10:29 +0900

    add react-sequence-diagram

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import './Custom.css';
 
+import SequenceDiagram from 'react-sequence-diagram';
+ 
+const input =
+  'Andrew->China: Says Hello\n' +
+  'Note right of China: China thinks\\nabout it\n' +
+  'China-->Andrew: How are you?\n' +
+  'Andrew->>China: I am good thanks!';
+ 
+const options = {
+  theme: 'simple'
+};
+
+function onError(error) {
+  console.log(error);
+}
+ 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <SequenceDiagram input={input} options={options} onError={onError} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
