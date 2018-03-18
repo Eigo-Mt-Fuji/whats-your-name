4a998ae6b01b3e854eaa7af48a66addca1bb7ec3
efg.river <efg.river@gmail.com>
2018-03-18 12:56:51 +0900

    fetch api呼び出しカ所を追加

---
+++ src/ApiHelper.js
@@ -0,0 +1,20 @@
+import 'whatwg-fetch';
+
+export default class ApiHelper {
+
+
+    constructor(base_url) {
+
+        this.base_url = base_url;
+    }
+
+    get() {
+
+        return fetch(this.base_url, {
+            credentials: "same-origin"
+        }).then((response)=> {
+
+            return response.json();
+        });
+    }
+};
