dc122a46be23982c2b4815b397d7e3ef386511fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-24 10:06:07 +0000

    Deploy Eigo-Mt-Fuji/whats-your-name to github.com/Eigo-Mt-Fuji/whats-your-name.git:gh-pages

--- src/App.js
+++ src/App.js
@@ -34,11 +34,11 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">テスト</h1>
                 </header>
                 <SequenceDiagram input={input} options={options} onError={onError}/>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+                    こんにちは。
                 </p>
                 <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
                     <MyPaper></MyPaper>
