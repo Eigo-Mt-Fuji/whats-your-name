94b3f7993f72117cb503f033ca0fb6428664d158
efg.river <efg.river@gmail.com>
2018-03-24 18:33:34 +0900

    uncomment fetch api testing

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Custom.css';
-
+import MyPaper from './components/MyPaper';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import SequenceDiagram from 'react-sequence-diagram';
 import ApiHelper from "./ApiHelper";
 
 const input =
-  'User->Slack: Type /maskcap command\n' +
-  'Note right of Slack: send https POST request to maskcap\'s endpoint\n' +
-  'Slack->AWSLambda: JSON\n' +
-  'Note right of AWS Lambda: Get channel\'s history (https://api.slack.com/channels/history), mask, and convert to markdown \n' +
-  'Lambda API->Github: Commit via jsgit npm\n' +
-  'Github->AWSLambda: (^_^)\n' +
-  'Lambda API->Slack: JSON response\n' +
-  'Slack->User: Show result in (the user only)\n';
- 
+    'User->Slack: Type /maskcap command\n' +
+    'Note right of Slack: send https POST request to maskcap\'s endpoint\n' +
+    'Slack->AWSLambda: JSON\n' +
+    'Note right of AWS Lambda: Get channel\'s history (https://api.slack.com/channels/history), mask, and convert to markdown \n' +
+    'Lambda API->Github: Commit via jsgit npm\n' +
+    'Github->AWSLambda: (^_^)\n' +
+    'Lambda API->Slack: JSON response\n' +
+    'Slack->User: Show result in (the user only)\n';
+
 const options = {
-  theme: 'simple'
+    theme: 'simple'
 };
 
 function onError(error) {
-  console.log(error);
+    console.log(error);
 }
- 
+
 class App extends Component {
-  render() {
+    render() {
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <SequenceDiagram input={input} options={options} onError={onError} />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  };
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <SequenceDiagram input={input} options={options} onError={onError}/>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                    <MyPaper></MyPaper>
+                </MuiThemeProvider>
+            </div>
+        );
+    };
 
-  componentDidMount() {
-
-      let helper = new ApiHelper("/ip.json");
-      helper.get().then((json) => {
-
-          console.log("Response", json);
-      });
-  }
+    // componentDidMount() {
+    //
+    //     let helper = new ApiHelper("/ip.json");
+    //     helper.get().then((json) => {
+    //
+    //         console.log("Response", json);
+    //     });
+    // }
 }
 
 export default App;
