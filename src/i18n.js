df92ca2b3be82f4c1516bf561e9b2c1bc5ffabcd
efg.river <efg.river@gmail.com>
2018-04-06 16:06:51 +0900

    fix i18n transitions.json not found error

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,8 @@ import i18n from "i18next";
 import Backend from "i18next-xhr-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import {reactI18nextModule} from "react-i18next";
+import enResources from "../public/locales/en/translations";
+import jaResources from "../public/locales/ja/translations";
 
 i18n
   .use(Backend)
@@ -14,8 +16,20 @@ i18n
     // have a common namespace used around the full app
     ns: ["translations"],
     defaultNS: "translations",
-    
-    debug: true,
+    backend: {
+      crossDomain: false,
+      ajax: function(url, options, callback) {
+        
+        if(url == "/locales/ja/translations.json") {
+          
+          callback(JSON.stringify(jaResources), {status: "200"});
+        }else {
+          
+          callback(JSON.stringify(enResources), {status: "200"});
+        }
+      }
+    },
+    debug: false,
     
     interpolation: {
       escapeValue: false, // not needed for react!!
